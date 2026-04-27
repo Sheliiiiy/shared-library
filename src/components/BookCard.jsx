8230982d861ffb757e7eba5facb01c2ff91e23a8
Sheliiiy <gabruci@gmail.com>
2026-04-27 23:58:53 +0300

    na

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -93,7 +93,6 @@ export default function BookCard({ book, onDelete, onUpdateBook }) {
                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
                 <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
               </svg>
-              Remove
             </button>
           </div>
         </div>
